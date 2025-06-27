2094b80c5b4af005e254c562c9011d03173d379b
Vishu-348 <vinodeet.dewangan05@outlook.com>
2025-06-28 01:48:36 +0530

    Add files via upload

---
+++ Module File handling/01_read.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *ptr;
+    ptr = fopen("top.txt", "r");
+   
+    while(1){
+        char ch = fgetc(ptr);
+        printf("%c\n",ch);
+        
+        if(ch == EOF){
+            break;
+        }
+    }
+
+    fclose(ptr);
+    
+    return 0;
+}
